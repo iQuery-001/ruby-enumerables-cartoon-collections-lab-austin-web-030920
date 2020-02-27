9ad943220767d10a49d771789e28d303c4127c24
Dakota Willingham <kota@vaga.tk>
2020-02-27 21:37:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any{|e| e.length > 4}
+  array.any>{|e| e.length > 4}
 end
 
 def find_the_cheese# code an argument here
