885d51352715bd8c4689acf477bc1037f9c3c643
Dakota Willingham <kota@vaga.tk>
2020-02-27 21:27:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |e| puts "#{e.capitalize()}!"  }
+  array.map { |e| puts "#{e}!"  }
 end
 
 def long_planeteer_calls# code an argument here
